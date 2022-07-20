d379809935689a521398c89ca72fb90f2e2a151d
Helton Pereira <helton.bfsm@gmail.com>
2022-07-20 18:25:07 -0300

    css aplicado

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Title from './Title';
 import PlanetCard from './PlanetCard';
 import planets from '../data/planets';
+import '../App.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div data-testid="solar-system" className="container">
         <Title headline="Planetas" />
-        <div>
+        <div className="planets-display">
           {planets.map((e, i) => (
             <PlanetCard
               key={ i }
