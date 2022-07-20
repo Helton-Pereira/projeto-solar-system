4e0c491260cbfd559dd1c88fabe48538de1dc61e
Helton Pereira <helton.bfsm@gmail.com>
2022-07-20 16:44:53 -0300

    Requisitos 6, 7 e 8 cumpridos

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div>
+          {planets.map((e, i) => (
+            <PlanetCard
+              key={ i }
+              planetName={ e.name }
+              planetImage={ e.image }
+            />))}
+        </div>
       </div>
     );
   }
