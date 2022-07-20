5136d96bf054401a513815591d8c2e42885edca1
Helton Pereira <helton.bfsm@gmail.com>
2022-07-20 17:13:57 -0300

    Requisitos 9 e 10 cumpridos

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <div>
+          {missions.map((e, i) => (
+            <MissionCard
+              key={ i }
+              name={ e.name }
+              year={ e.year }
+              country={ e.country }
+              destination={ e.destination }
+            />))}
+        </div>
       </div>
     );
   }
